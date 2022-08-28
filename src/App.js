0a8cf013d5f89b52c99ed2792e889e59ffacd244
Correct Name <nurzhan.murzakhmet@gmail.com>
2022-08-28 17:24:14 +0300

    component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import './App.css';
 function App() {
   return (
     <div>
-      <div>one div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header />
+      <Technologies />
     </div>
 
   );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a>Home</a>
+      <a>News Feed</a>
+      <a>Messages</a>
+    </div>
+  )
+}
+
+const Technologies = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App;
