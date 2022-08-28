ca9dbf41cab583cea57b47c28a2a5186697f02a4
Correct Name <nurzhan.murzakhmet@gmail.com>
2022-08-28 20:10:13 +0600

    refactoring app.jsx

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="Logo" />
-      </header>
-      <nav className="nav">
-        <div>
-          Profile
-        </div>
-        <div>
-          Messages
-        </div>
-        <div>
-          News
-        </div>
-        <div>
-          Music
-        </div>
-        <div>
-          Settings
-        </div>
-      </nav>
-      <div className="content">
-        Main content
-        <div>
-          <img src="https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320" alt="Background" />
-        </div>
-        <div>
-          ava + description
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-          <div>
-            <div>post 1</div>
-            <div>post 2</div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
 
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="content">
+        Main content
+        <div>
+        <img src="https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320" alt="Background" />
+        </div>
+        <div>
+        ava + description
+        </div>
+        <div>
+        My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
