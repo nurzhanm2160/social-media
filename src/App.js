866c02f9f7bc20935280a99e300f519c9db908d2
Correct Name <nurzhan.murzakhmet@gmail.com>
2022-08-29 18:44:58 +0600

    react-router-dom, routings

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
@@ -10,8 +11,10 @@ function App() {
       <Header />
       <Navbar />
       <div className="content">
-        {/* <Profile /> */}
-        <Dialogs />
+        <Routes>
+          <Route path='profile' element={<Profile />}/>
+          <Route path='dialogs' element={<Dialogs />} />
+        </Routes>
       </div>
     </div>
 
